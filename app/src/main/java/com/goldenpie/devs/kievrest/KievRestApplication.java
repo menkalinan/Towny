027263fe3b0267a633974d72feeffb7b4bd3cbfc
antoxa2584x <antoxa2584@gmail.com>
2015-09-23 00:06:55 +0300

    Done with Fabric

--- app/src/main/java/com/goldenpie/devs/kievrest/KievRestApplication.java
+++ app/src/main/java/com/goldenpie/devs/kievrest/KievRestApplication.java
@@ -2,10 +2,12 @@ package com.goldenpie.devs.kievrest;
 
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.goldenpie.devs.kievrest.config.AppComponent;
 import com.goldenpie.devs.kievrest.config.AppModule;
 import com.goldenpie.devs.kievrest.config.DaggerAppComponent;
 
+import io.fabric.sdk.android.Fabric;
 import lombok.Getter;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
