900e9eb0e2788ccdc1bcc7cdc3f5d2b571a96736
antoxa2584x <antoxa2584@gmail.com>
2015-10-03 20:30:38 +0300

    Work on Wearable extension

---
+++ constantskeeper/src/main/java/com/goldenpie/devs/constanskeeper/Constants.java
@@ -0,0 +1,5 @@
+package com.goldenpie.devs.constanskeeper;
+
+public class Constants {
+    public static final String FIND_NEAREST_PLACES = "get_places_near_my_location";
+}
---
+++ wear/src/main/java/com/goldenpie/devs/keivrestwear/PlacesSyncHelper.java
@@ -0,0 +1,14 @@
+package com.goldenpie.devs.keivrestwear;
+
+import com.google.android.gms.wearable.DataMap;
+import com.mariux.teleport.lib.TeleportClient;
+
+public class PlacesSyncHelper extends TeleportClient.OnSyncDataItemCallback {
+    public PlacesSyncHelper(TeleportClient mTeleportClient) {
+    }
+
+    @Override
+    public void onDataSync(DataMap dataMap) {
+
+    }
+}
