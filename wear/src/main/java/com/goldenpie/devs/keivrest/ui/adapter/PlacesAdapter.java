9319349cecb073c29251f9a633c637e80d3c1d6b
antoxa2584x <antoxa2584@gmail.com>
2015-10-05 16:35:34 +0300

    Fix random spacing of ListView elements

--- wear/src/main/java/com/goldenpie/devs/keivrest/ui/adapter/PlacesAdapter.java
+++ wear/src/main/java/com/goldenpie/devs/keivrest/ui/adapter/PlacesAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.goldenpie.devs.keivrest.model.PlacesModel;
+import com.goldenpie.devs.keivrest.ui.view.WearableListItemLayout;
 import com.goldenpie.devs.kievrest.R;
 
 import lombok.Getter;
@@ -29,15 +30,18 @@ public class PlacesAdapter extends WearableListView.Adapter {
 
     @Override
     public WearableListView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        return new ViewHolder(inflater.inflate(R.layout.adapter_place_item, null));
+        return new WearableListView.ViewHolder(new WearableListItemLayout(getContext()));
     }
 
     @Override
     public void onBindViewHolder(WearableListView.ViewHolder holder, int position) {
-        ViewHolder itemHolder = (ViewHolder) holder;
+        WearableListItemLayout view = (WearableListItemLayout) holder.itemView;
 
-        itemHolder.address.setText(models.getAddress().get(position));
-        itemHolder.label.setText(models.getLabels().get(position));
+        TextView label = (TextView) view.findViewById(R.id.adp_place_item_title);
+        TextView address = (TextView) view.findViewById(R.id.adp_place_item_address);
+
+        address.setText(models.getAddress().get(position));
+        label.setText(models.getLabels().get(position));
 
         holder.itemView.setTag(position);
     }
@@ -47,13 +51,4 @@ public class PlacesAdapter extends WearableListView.Adapter {
         return models.getIds().size();
     }
 
-    public static class ViewHolder extends WearableListView.ViewHolder {
-        public TextView label;
-        public TextView address;
-        ViewHolder(View view) {
-            super(view);
-            label = (TextView) view.findViewById(R.id.adp_place_item_title);
-            address = (TextView) view.findViewById(R.id.adp_place_item_address);
-        }
-    }
 }
--- wear/src/main/java/com/goldenpie/devs/keivrest/ui/view/WearableListItemLayout.java
+++ wear/src/main/java/com/goldenpie/devs/keivrest/ui/view/WearableListItemLayout.java
@@ -4,28 +4,30 @@ package com.goldenpie.devs.keivrest.ui.view;
 import android.content.Context;
 import android.support.wearable.view.WearableListView;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.goldenpie.devs.kievrest.R;
 
-public class WearableListItemLayout extends LinearLayout
+public class WearableListItemLayout extends FrameLayout
              implements WearableListView.OnCenterProximityListener {
 
     private TextView title;
+    private TextView descriprion;
 
     public WearableListItemLayout(Context context) {
-        this(context, null);
+        super(context);
+        init(context);
     } 
- 
-    public WearableListItemLayout(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    } 
- 
-    public WearableListItemLayout(Context context, AttributeSet attrs,
-                                  int defStyle) {
-        super(context, attrs, defStyle);
-     }
+
+    private void init(Context context) {
+        View.inflate(context, R.layout.adapter_place_item, this);
+        title = (TextView) findViewById(R.id.adp_place_item_address);
+        descriprion = (TextView) findViewById(R.id.adp_place_item_title);
+    }
+
 
     @Override 
     protected void onFinishInflate() { 
@@ -35,11 +37,15 @@ public class WearableListItemLayout extends LinearLayout
  
     @Override 
     public void onCenterPosition(boolean animate) {
-        title.setTextColor(getResources().getColor(R.color.primary_dark));
+//        title.setTextColor(getResources().getColor(R.color.primary_dark));
+        title.animate().scaleX(1f).scaleY(1f).alpha(1);
+        descriprion.animate().scaleX(1f).scaleY(1f).alpha(1);
     }
  
     @Override 
     public void onNonCenterPosition(boolean animate) {
-        title.setTextColor(getResources().getColor(R.color.primary_text));
+//        title.setTextColor(getResources().getColor(R.color.primary_text));
+        title.animate().scaleX(0.8f).scaleY(0.8f).alpha(0.6f);
+        descriprion.animate().scaleX(0.8f).scaleY(0.8f).alpha(0.6f);
     } 
 } 
